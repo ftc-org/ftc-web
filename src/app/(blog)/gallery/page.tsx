230dcfe61ff844d56bcf947fec97a7c1b5c93a39
Albert <albert.dugba@gmail.com>
2024-10-03 09:53:12 +0100

    feat: add sitemap

--- src/app/(blog)/gallery/page.tsx
+++ src/app/(blog)/gallery/page.tsx
@@ -7,13 +7,13 @@ import { ImageMasonryLayout } from "@/app/components/masonry-layout";
 
 const GalleryPage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className='min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8'>
       <ImageMasonryLayout
         mediaItems={[...mediaItems, ...PATRIOTS]}
-        showVideos={false}
-        videoFrameClassName="lg:h-60 h-50"
-        title="Galamsey in Ghana: A Visual Journey"
-        subtitle="Exploring the impact of illegal small-scale gold mining"
+        showVideos={true}
+        videoFrameClassName='lg:h-60 h-50'
+        title='Galamsey in Ghana: A Visual Journey'
+        subtitle='Exploring the impact of illegal small-scale gold mining'
       />
     </div>
   );
